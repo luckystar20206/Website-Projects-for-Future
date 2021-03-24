45d2a4ea883229d27abaac6b35a9a74ca7c381d5
luckystar20206 <luckystar20206@gmail.com>
2021-03-24 22:54:54 +0100

    Unterstützen Knopf hinzugefügt. Upvote Downvote Knopf verändert. Teilen Knopf hinzugefügt. Muss noch den Teilen Knopf irgendwie nach rechts bringen.

--- src/components/ActiveProjects.vue
+++ src/components/ActiveProjects.vue
@@ -3,136 +3,133 @@
     <h1 class="subheading grey--text">Aktive Projekte</h1>
 
     <v-container class="my-5">
-      <v-data-iterator
-          :items="projects"
-          :items-per-page="5"
-          item-key="title"
-          show-select
-          class="elevation-1"
-      >
-      <v-toolbar  color = "deep-purple accent-4" dense dark class ="mb-6">
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        <v-toolbar-title>
-          Aktive Projekte
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-text-field
-            v-model="filter"
-            type="text"
-            clearable
-            flat
-            solo-inverted
-            hide-details
-            prepend-inner-icon="mdi-magnify"
-            label="Suche nach Projekten"
-            class ="shrink"
-        ></v-text-field>
-        <v-menu
-            bottom
-            left
-            rounded = "lg"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-                dark
-                text
-                v-bind="attrs"
-                v-on="on"
-            >aufsteigend
-            </v-btn>
-          </template>
-          <v-list>
-            <v-list-item
-                v-for="property in sort"
-                :key="property.name"
-                link
-            >
-              <v-list-item-icon>
-                <v-icon>{{property.icon}}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title @click="sortByAsc(property.sortProperty)">{{property.name}}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-        <v-menu
-            bottom
-            left
-            rounded = "lg"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-                dark
-                text
-                v-bind="attrs"
-                v-on="on"
-            >absteigend
-            </v-btn>
-          </template>
-          <v-list>
-            <v-list-item
-                v-for="property in sort"
-                :key="property.name"
-                link
-            >
-              <v-list-item-icon>
-                <v-icon>{{property.icon}}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title @click="sortByDesc(property.sortProperty)">{{property.name}}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </v-toolbar>
-      <v-layout row wrap>
-        <v-flex xs12 sm6 md4 lg3 v-for="project in filteredProjects" :key="project.title">
-          <v-card class="ma-3">
-            <v-responsive class="text-center pt-4">
-              <v-avatar size ="100">
-                <img :src = "project.img" alt="">
-              </v-avatar>
-            </v-responsive>
-            <v-card-text>
-              <div class ="text-center headline">{{project.title}}</div>
-              <v-progress-linear
-                  color="green"
-                  height="12"
-                  :value ="getProgress(project)"
+        <v-toolbar  color = "deep-purple accent-4" dense dark class ="mb-6">
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+          <v-toolbar-title>
+            Aktive Projekte
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-text-field
+              v-model="filter"
+              type="text"
+              clearable
+              flat
+              solo-inverted
+              hide-details
+              prepend-inner-icon="mdi-magnify"
+              label="Suche nach Projekten"
+              class ="shrink"
+          ></v-text-field>
+          <v-menu
+              bottom
+              left
+              rounded = "lg"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                  dark
+                  text
+                  v-bind="attrs"
+                  v-on="on"
+              >aufsteigend
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                  v-for="property in sort"
+                  :key="property.name"
+                  link
               >
-                <!-- value = project.progress irgendwie setzen, ist aber keine zahl!-->
-                <strong>{{project.amount}}€ von {{project.needs}}€</strong>
-
-              </v-progress-linear>
-              <div class = ma-1><span class = "text-md-subtitle-2 black--text font-weight-bold" >Ziel: </span> <span class = grey--text>{{project.goal}}</span></div>
-              <div class = ma-1><span class = "text-md-subtitle-2 black--text font-weight-bold" >Finanzbedarf: </span><span class = grey--text>{{project.needs}}€</span></div>
-
-              <vue-countdown :time= "getRemainingTime(project)" :transform="transformSlotProps" v-slot="{ days, hours, minutes, seconds}">
-                <div class = ma-1> <span class = "text-md-subtitle-2 black--text font-weight-bold" >Restzeit：</span> <span class = grey--text>{{ days }} Tage, {{hours}}:{{ minutes }}:{{ seconds }}.</span></div>
-              </vue-countdown>
-
-              <div class = ma-1><span class = "text-md-subtitle-2 black--text font-weight-bold" >Unterstützer: </span> <span class = grey--text>{{project.supporterCount}}</span></div>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn flat color ="grey">
-                <v-icon small left>thumb_up</v-icon>
-                <span class ="">Upvote</span>
+                <v-list-item-icon>
+                  <v-icon>{{property.icon}}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title @click="sortByAsc(property.sortProperty)">{{property.name}}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+          <v-menu
+              bottom
+              left
+              rounded = "lg"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                  dark
+                  text
+                  v-bind="attrs"
+                  v-on="on"
+              >absteigend
               </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                  v-for="property in sort"
+                  :key="property.name"
+                  link
+              >
+                <v-list-item-icon>
+                  <v-icon>{{property.icon}}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title @click="sortByDesc(property.sortProperty)">{{property.name}}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar>
+        <v-layout row wrap>
+          <v-flex xs12 sm6 md4 lg3 v-for="project in filteredProjects" :key="project.title">
+            <v-card class="ma-3">
+              <v-responsive class="text-center pt-4">
+                <v-avatar size ="100">
+                  <img :src = "project.img" alt="">
+                </v-avatar>
+              </v-responsive>
+              <v-card-text>
+                <div class ="text-center headline">{{project.title}}</div>
+                <v-progress-linear
+                    color="green"
+                    height="12"
+                    :value ="getProgress(project)"
+                >
+                  <strong>{{project.amount}}€ von {{project.needs}}€</strong>
+
+                </v-progress-linear>
+                <div class = ma-1><span class = "text-md-subtitle-2 black--text font-weight-bold" >Ziel: </span> <span class = grey--text>{{project.goal}}</span></div>
+                <div class = ma-1><span class = "text-md-subtitle-2 black--text font-weight-bold" >Finanzbedarf: </span><span class = grey--text>{{project.needs}}€</span></div>
+
+                <vue-countdown :time= "getRemainingTime(project)" :transform="transformSlotProps" v-slot="{ days, hours, minutes, seconds}">
+                  <div class = ma-1> <span class = "text-md-subtitle-2 black--text font-weight-bold" >Restzeit：</span> <span class = grey--text>{{ days }} Tage, {{hours}}:{{ minutes }}:{{ seconds }}.</span></div>
+                </vue-countdown>
+
+                <div class = ma-1><span class = "text-md-subtitle-2 black--text font-weight-bold" >Unterstützer: </span> <span class = grey--text>{{project.supporterCount}}</span></div>
+              </v-card-text>
               <v-spacer></v-spacer>
-              <v-btn flat color ="grey">
-                <v-icon small left>thumb_down</v-icon>
-                <span class ="">Downvote</span>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-          <template v-slot:bottom="{pagination, options, updateOptions }">
-          <v-data-footer
-              :pagination="pagination"
-              :options="options"
-              :items-per-page-options="itemsPerPage"
-              @update:options="updateOptions"
-              items-per-page-text="$vuetify.dataTable.itemsPerPageText"/>
-          </template>
-        </v-flex>
-      </v-layout>
-      </v-data-iterator>
+              <v-card-actions>
+                <v-col class ="text-center">
+                  <v-btn class="text-center green accent-4 white--text">
+                    <v-icon small left>euro_symbol</v-icon>
+                    <span class ="">Unterstützen</span>
+                   <v-icon small right>euro_symbol</v-icon>
+                 </v-btn>
+                </v-col>
+              </v-card-actions>
+<!--              Das Teilen Icon irgendwie nach rechts bringen-->
+              <v-col>
+                <v-btn icon left>
+                  <v-icon>thumb_up</v-icon>
+                </v-btn>
+                <span class="text-md-subtitle-2 black--text font-weight-bold ">{{project.upvoteCount}}</span>
+                <v-btn icon>
+                  <v-icon>thumb_down</v-icon>
+                </v-btn>
+                <span class="text-md-subtitle-2 black--text font-weight-bold ">{{project.downvoteCount}}</span>
+                <v-btn icon right>
+                  <v-icon>share</v-icon>
+                </v-btn>
+                <span class="text-md-subtitle-2 black--text font-weight-bold">Teilen</span>
+              </v-col>
+            </v-card>
+          </v-flex>
+        </v-layout>
     </v-container>
 
   </div>
@@ -160,9 +157,9 @@ export default {
           goal:"Die karge Landschaft zu verschönern und alles zu retten und grüner zu machen lorem ipsum...",
           needs:400,
           due:new Date(2021,7,1,2,21),
-          supporterCount: 53,
-          upvoteCount: 0,
-          downvoteCount:0,
+          supporterCount: 0,
+          upvoteCount: 42,
+          downvoteCount:2,
           amount: 77,
           progress: (this.amount / this.needs),
         },
@@ -172,9 +169,57 @@ export default {
           goal:"Bienen retten",
           needs:230,
           due:new Date(2021,5,12,9,43),
-          supporterCount: 71,
-          upvoteCount: 0,
-          downvoteCount:0,
+          supporterCount: 0,
+          upvoteCount: 532,
+          downvoteCount:34,
+          amount: 150,
+          progress: (this.amount / this.needs),
+        },
+        {
+          title:"Blumen gießen 2",
+          img: "/avatar-2.png",
+          goal:"Bienen retten",
+          needs:230,
+          due:new Date(2021,5,12,9,43),
+          supporterCount: 0,
+          upvoteCount: 232,
+          downvoteCount:11,
+          amount: 150,
+          progress: (this.amount / this.needs),
+        },
+        {
+          title:"Blumen gießen 3",
+          img: "/avatar-2.png",
+          goal:"Bienen retten",
+          needs:230,
+          due:new Date(2021,5,12,9,43),
+          supporterCount: 0,
+          upvoteCount: 31,
+          downvoteCount:5,
+          amount: 150,
+          progress: (this.amount / this.needs),
+        },
+        {
+          title:"Blumen gießen 4",
+          img: "/avatar-2.png",
+          goal:"Bienen retten",
+          needs:230,
+          due:new Date(2021,5,12,9,43),
+          supporterCount: 0,
+          upvoteCount: 125,
+          downvoteCount:24,
+          amount: 150,
+          progress: (this.amount / this.needs),
+        },
+        {
+          title:"Blumen gießen 5",
+          img: "/avatar-2.png",
+          goal:"Bienen retten",
+          needs:230,
+          due:new Date(2021,5,12,9,43),
+          supporterCount: 0,
+          upvoteCount: 42,
+          downvoteCount:13,
           amount: 150,
           progress: (this.amount / this.needs),
         },
@@ -184,9 +229,9 @@ export default {
           goal:"Projects for future",
           needs:1000,
           due:new Date(2021,8,12,4,58),
-          supporterCount: 4,
-          upvoteCount: 0,
-          downvoteCount:0,
+          supporterCount: 0,
+          upvoteCount: 24,
+          downvoteCount:2,
           amount: 45,
           progress: (this.amount / this.needs),
         },
@@ -196,9 +241,9 @@ export default {
           goal:"Die karge Landschaft zu verschönern",
           needs:400,
           due:new Date(2021,2,14,4,58),
-          supporterCount: 98,
-          upvoteCount: 0,
-          downvoteCount:0,
+          supporterCount: 0,
+          upvoteCount: 109,
+          downvoteCount:27,
           amount: 45,
           progress: (this.amount / this.needs),
         },
@@ -209,8 +254,8 @@ export default {
           needs:555,
           due:new Date(2021,5,14,4,38),
           supporterCount: 42,
-          upvoteCount: 0,
-          downvoteCount:0,
+          upvoteCount: 42,
+          downvoteCount:3,
           amount: 70,
           progress: (this.amount / this.needs),
         },
